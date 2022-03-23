2a671451517975834aaf218eed08eebdd939ff59
Gerald Navarette <gerald.navarette@gmail.com>
2022-03-23 10:42:48 +0100

    create data install bootstrap

---
+++ src/app/car.ts
@@ -0,0 +1,12 @@
+export interface Car {
+    id: string;
+    brand: string;
+    type: string;
+    power: number;
+    description: string;
+    price: string;
+    wifi: boolean;
+    image: string;
+}
+
+
---
+++ src/app/mock-cars.ts
@@ -0,0 +1,105 @@
+import { Car } from './car';
+
+export const ALBUMS: Car[] = [
+    {
+        "id": "1",
+        "brand": "Ford",
+        "type": "Mustang",
+        "power": 330,
+        "description": "L’incroyable Ford Mustang a récemment été qualifiée de voiture la plus performante jamais vendue en Europe",
+        "price": "500",
+        "wifi": true,
+        "image": ""
+    },
+    {
+        "id": "2",
+        "brand": "Ford",
+        "type": "Focus",
+        "power": 330,
+        "description": "Avec son nouveau design et sa connectivité dernière génération, la Ford Focus offre une expérience de conduite plus agréable que jamais.",
+        "price": "150",
+        "wifi": true,
+        "image": ""
+    },
+    {
+        "id": "3",
+        "brand": "Ford",
+        "type": "Fiesta",
+        "power": 100,
+        "description": "Grâce à son nouveau design extérieur, l'iconique citadine arrive à dégager une présence impressionante sur la route.",
+        "price": "130",
+        "wifi": true,
+        "image": ""
+    },
+    {
+        "id": "4",
+        "brand": "BMW",
+        "type": "Z4",
+        "power": 250,
+        "description": "Couvert ou décapoté, le plaisir de conduire ne connaît plus aucunes limites au volant de la nouvelle BMW Z4. Un roadster, dans sa version la plus aboutie ",
+        "price": "700",
+        "wifi": true,
+        "image": ""
+    },
+    {
+        "id": "5",
+        "brand": "BMW",
+        "type": "318",
+        "power": 150,
+        "description": "La 318 ouvre désormais l'offre essence de la berline star du constructeur bavarois, avec la bonne idée de conserver un 4 cylindres.",
+        "price": "400",
+        "wifi": true,
+        "image": ""
+    },
+    {
+        "id": "6",
+        "brand": "Renault",
+        "type": "Scenic",
+        "power": 100,
+        "description": "Le monospace à vivre. découvrez une voiture familiale compacte et innovante, maline dans son optimisation de l'espace intérieur.",
+        "price": "250",
+        "wifi": true,
+        "image": ""
+    },
+    {
+        "id": "7",
+        "brand": "Renault",
+        "type": "Megane",
+        "power": 90,
+        "description": "Corps de berline compacte, style crossover, arrière sculpté, lignes sportives. Ses lignes aérodynamiques offrent une allure racée, une conduite exaltante. ",
+        "price": "150",
+        "wifi": true,
+        "image": ""
+    },
+    {
+        "id": "8",
+        "brand": "Renault",
+        "type": "Clio",
+        "power": 80,
+        "description": "l'iconique citadine continue de vous surprendre",
+        "price": "100",
+        "wifi": false,
+        "image": ""
+    },
+    {
+        "id": "9",
+        "brand": "Dacia",
+        "type": "Duster",
+        "power": 90,
+        "description": "Nouveau Dacia Duster affirme son caractère de baroudeur pour vous accompagner dans toutes vos aventures et s'impose comme le SUV familial emblématique Dacia.",
+        "price": "200",
+        "wifi": true,
+        "image": ""
+    },
+    {
+        "id": "10",
+        "brand": "Dacia",
+        "type": "Sandero",
+        "power": 80,
+        "description": "Avec sa silhouette robuste et sa face avant repensée, soulignée par sa signature de marque en forme de Y à feux LED, Sandero dévoile son design totalement renouvelé.",
+        "price": "150",
+        "wifi": false,
+        "image": ""
+    }
+
+]
